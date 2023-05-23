ece072cd3d2bd7eff41811f6053a19234cf50692
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-05-23 16:27:33 +0100

    functioning app

--- WeatherOrNot/src/App.jsx
+++ WeatherOrNot/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Header from './Header'
 import WeatherContent from './WeatherContent'
-import video from './assets/background.mp4'
+// import video from './assets/background.mp4'
 
 function App() {
 
   return (
     <main className="whole" alt="rolling clowds on a light blue sky">
       <section className="background">
-        <video id="myVideo" autoPlay={"autoplay"} muted preLoad="auto" loop ><source src={video} type="video/mp4" alt="moving blue particles swirling on a light blue background"/></video>
+        {/* <video id="myVideo" autoPlay={"autoplay"} muted preLoad="auto" loop ><source src={video} type="video/mp4" alt="moving blue particles swirling on a light blue background"/></video> */}
       </section>
       <section className = "content-box">
         <Header />
