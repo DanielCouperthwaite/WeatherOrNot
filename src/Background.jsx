3e6b1b0e81d460e3627ca238e70aa0cbe4390216
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-06-29 13:38:18 +0100

    remove video

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,10 +1,12 @@
 import video from './assets/clouds.mp4'
+import cloud from './assets/cloud.jpg'
 
 export default function Background () {
     return (
         <>
         <section className="background">
-            <video id="myVideo" autoPlay={"autoplay"} muted preLoad="auto" loop ><source src={video} type="video/mp4" alt="moving blue particles swirling on a light blue background"/></video>
+            {/* <video id="myVideo" autoPlay={"autoplay"} muted preLoad="auto" loop ><source src={video} type="video/mp4" alt="moving blue particles swirling on a light blue background"/></video> */}
+            <image src={cloud}></image>
         </section>
         </>
     )
